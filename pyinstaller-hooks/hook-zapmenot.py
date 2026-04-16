89853f3ec4fcb1605fa3845a3972b8586f50af80
Alan Ford <alanford354@gmail.com>
2026-04-16 14:45:22 -0400

    Cleanup for pyinstaller

--- pyinstaller-hooks/hook-zapmenot.py
+++ pyinstaller-hooks/hook-zapmenot.py
@@ -1,6 +1,4 @@
 # pyinstaller-hooks/hook-zapmenot.py
-from PyInstaller.utils.hooks import collect_submodules, collect_data_files,
-    collect_dynamic_libs
+from PyInstaller.utils.hooks import collect_submodules, collect_data_files
 hiddenimports = collect_submodules('zapmenot')
 datas = collect_data_files('zapmenot')
-# binaries = collect_dynamic_libs('zapmenot')
