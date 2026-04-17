234dac127d8f92be77700e4ed865bc2a1e5ff217
Alan Ford <alanford354@gmail.com>
2026-04-17 10:01:50 -0400

    Save the ZapMeNot script

--- src/ZapMeNotQt/ScriptDisplayDialog.py
+++ src/ZapMeNotQt/ScriptDisplayDialog.py
@@ -1,6 +1,6 @@
 import os
 
-from PyQt6.QtWidgets import QDialog
+from PyQt6.QtWidgets import QDialog, QFileDialog
 from PyQt6.QtCore import QFile, QIODeviceBase
 from PyQt6 import uic
 from pathlib import Path
@@ -10,11 +10,12 @@ class ScriptDisplayDialog(QDialog):
     def __init__(self, text_list):
         super().__init__()
         self.load_ui()
-        display_text = ""
+        self.display_text = ""
         for entry in text_list:
-            display_text += entry + "\n"
-        self.textEdit.setText(display_text)
+            self.display_text += entry + "\n"
+        self.textEdit.setText(self.display_text)
         self.pushButton.clicked.connect(self.close)
+        self.pushButton_2.clicked.connect(self.saveFile)
 
     def load_ui(self):
         path = os.fspath(Path(__file__).resolve().parent /
@@ -23,3 +24,16 @@ class ScriptDisplayDialog(QDialog):
         ui_file.open(QIODeviceBase.OpenModeFlag.ReadOnly)
         uic.loadUi(ui_file, self)
         ui_file.close()
+
+    def saveFile(self):
+        # Open the save file dialog
+        filename, _ = QFileDialog.getSaveFileName(None, 
+                                                  "Save File", 
+                                                  "", 
+                                                  "Python Files (*.py);;All Files (*)")
+
+        if filename:
+            # Proceed to save the file using the selected filename
+            with open(filename, 'w') as file:
+                file.write(self.display_text)
+            file.close()
